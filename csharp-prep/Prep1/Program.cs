4321d71c7e2acbbb25a0d58bc89276fdcabe77c9
SCCann0n <1s.cannon.1s@gmail.com>
2025-01-13 23:07:01 -0700

    Moved code to correct files

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,50 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your grade? ");
-        string response = Console.ReadLine();
-        int gradePercent = int.Parse(response);
+        Console.Write("What is your first name? ");
+string FirstName = Console.ReadLine();
 
-        string letter = "";
+Console.Write("What is your last name? ");
+string LastName = Console.ReadLine();
 
-        if (gradePercent < 60)
-        {
-            letter = "F";
-        }
-        if (gradePercent >= 60 && gradePercent < 70)
-        {
-            letter = "D";
-        }
-        if (gradePercent >= 70 && gradePercent < 80)
-        {
-            letter = "C";
-        }
-        if (gradePercent >= 80 && gradePercent < 90)
-        {
-            letter = "B";
-        }
-        if (gradePercent >= 90)
-        {
-            letter = "A";
-        }
-
-        Console.WriteLine($"Your letter grade is {letter}!");
-
-        if (gradePercent >= 70)
-        {
-            Console.WriteLine("You passed!");
-        }
-        else
-        {
-            Console.WriteLine("You'll get it next time!");
-        }
+Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}. ");
     }
 }
-
-// Console.Write("What is your first name? ");
-// string FirstName = Console.ReadLine();
-
-// Console.Write("What is your last name? ");
-// string LastName = Console.ReadLine();
-
-// Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}. ");
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string response = Console.ReadLine();
+        int gradePercent = int.Parse(response);
+
+        string letter = "";
+
+        if (gradePercent < 60)
+        {
+            letter = "F";
+        }
+        if (gradePercent >= 60 && gradePercent < 70)
+        {
+            letter = "D";
+        }
+        if (gradePercent >= 70 && gradePercent < 80)
+        {
+            letter = "C";
+        }
+        if (gradePercent >= 80 && gradePercent < 90)
+        {
+            letter = "B";
+        }
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}!");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You'll get it next time!");
+        }
     }
 }
