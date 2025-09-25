302ce5a2aac4974d788c3a4e2b2686e6ae520b66
SCCann0n <1s.cannon.1s@gmail.com>
2025-09-24 21:24:01 -0600

    Prep 3 Assignment Completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int trueNumber = int.Parse(magicNumber);
+
+        Console.Write("What is your guess? ");
+        string userGuess = Console.ReadLine();
+        int trueGuess = int.Parse(userGuess);
+
+
+        while (trueGuess != trueNumber)
+        {
+            if (trueGuess > trueNumber)
+                Console.WriteLine("Too high ");
+
+            else if (trueGuess < trueNumber)
+                Console.WriteLine("Too low ");
+
+            Console.Write("What is your guess? ");
+            userGuess = Console.ReadLine();
+            trueGuess = int.Parse(userGuess);
+        }
+        
+        Console.WriteLine("That's it! ");
     }
 }
